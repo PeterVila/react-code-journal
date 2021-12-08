154097abf7046f02e92e8d92e8b2684489bd1770
Peter Vila <peterdvila@gmail.com>
2021-12-07 23:28:48 -0800

    Revert "Added component for adding new entries. Used useReducer hook for all …"

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,61 +1,14 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState } from 'react';
 
 export default function Home(props) {
+  const [counter, addCounter] = useState(0);
+  const increment = () => {
+    addCounter(counter + 1);
+  };
   return (
-    <div className="entry-page">
-      <h1>New Entry</h1>
-      <div className="container">
-        <div className="row">
-          <div className="placeholder"></div>
-          <div className="inputs">
-            <Inputs />
-            <button>Submit</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'title':
-      return {
-        ...state,
-        Title: action.Title
-      };
-    case 'link':
-      return {
-        ...state,
-        Link: action.Link
-      };
-    case 'image':
-      return {
-        ...state,
-        Image: action.Image
-      };
-    case 'description':
-      return {
-        ...state,
-        Description: action.Description
-      };
-    default:
-      return state; // If the action isn't any of these
-  }
-};
-
-function Inputs(props) {
-  const [state, dispatch] = useReducer(reducer, {});
-  return (
-    <form>
-      <label>Title</label>
-      <input type="text" onChange={event => { dispatch({ type: 'title', Title: event.target.value }); }}>{state.title}</input>
-      <label>Link</label>
-      <input type="text" onChange={() => { dispatch({ type: 'link', Link: event.target.value }); }}>{state.link}</input>
-      <label>Image Upload</label>
-      <input type="text" onChange={() => { dispatch({ type: 'image', Image: event.target.value }); }}>{state.image}</input>
-      <label>Description</label>
-      <textarea onChange={() => { dispatch({ type: 'description', Description: event.target.value }); }}>{state.description}</textarea>
-    </form>
+    <>
+      <h1>{counter}</h1>
+      <button onClick={increment}>Add</button>
+    </>
   );
 }
