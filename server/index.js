2c9353f91cbecd399036bba3766f799e2039230a
Peter Vila <peterdvila@gmail.com>
2021-12-09 19:41:23 +0000

    Set up postgreSQL database server, filled in dummy data

--- server/index.js
+++ server/index.js
@@ -5,9 +5,31 @@ const staticMiddleware = require('./static-middleware');
 
 const app = express();
 
+const pg = require('pg');
+// only create ONE pool for your whole server
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+app.use(express.json());
 app.use(staticMiddleware);
-
 app.use(errorMiddleware);
+
+// Routes
+
+app.get('/api/entries', (req, res, next) => {
+  const sql = `
+  select *
+    from "entries"
+  `;
+  db.query(sql)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => next(err));
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
