b554f50b135a169b957abef35ca43cd67fc5a7bc
Peter Vila <peterdvila@gmail.com>
2021-12-12 01:15:31 +0000

    Installed react-router-dom dependencies, Adding routing to our 2 components within our SPA

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
@@ -7,8 +8,8 @@ export default function Navbar() {
       <div className="background navbar-row">
         <h1>Code Journal</h1>
         <input type="text"></input>
-        <h2>Create</h2>
-        <h2>View</h2>
+        <h2><Link to="/home">Create</Link></h2>
+        <h2><Link to="/">View</Link></h2>
       </div>
     </div>
   );
